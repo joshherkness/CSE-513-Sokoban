19dd9c287c7e215e2dc22efd6f084c32ebf7ab08
joshherkness <joshherkness@gmail.com>
2017-04-11 22:03:56 -0400

    Add function to determine simple deadlocks

--- services/state_helper.py
+++ services/state_helper.py
@@ -14,7 +14,6 @@ class StateHelper(object):
         neighbor_board_cells = StateHelper.get_neighbor_board_cells(board,
                                                                     pusher_position,
                                                                     direction)
-        
         n_0 = neighbor_board_cells['near']
         n_1 = neighbor_board_cells['far']
 
@@ -31,16 +30,33 @@ class StateHelper(object):
             print("IllegalMoveError risen!")
 
     @staticmethod
-    def get_neighbor_board_cells(board, position, direction):
+    def get_neighbor_cells(board, position, direction):
         '''
         retrieves the board cells in the nearest two locations in a direction
         of a position.
         '''
 
-        neighbor_positions = {'near': 0, 'far': 0}
-        neighbor_positions['near'] = board.neighbor(position,
-                                                    direction)
-        neighbor_positions['far'] = board.neighbor(neighbor_positions['near'],
-                                                   direction)
-        neighbor_board_cells = {k: board.__getitem__(v) for k, v in neighbor_positions.items()}
-        return neighbor_board_cells
+        near, far = StateHelper.get_neighbor_positions(board, position, direction)
+        try:
+            near_cell = board.__getitem__(near)
+        except:
+            near_cell = None
+
+        try:
+            far_cell = board.__getitem__(far)
+        except:
+            far_cell = None
+
+        return near, far, near_cell, far_cell
+
+    @staticmethod
+    def get_neighbor_positions(board, position, direction):
+        '''
+        retrieves the board cells in the nearest two locations in a direction
+        of a position.
+        '''
+
+        near = board.neighbor(position, direction)
+        far = board.neighbor(near, direction)
+        return near, far
+
--- sokoban.py
+++ sokoban.py
@@ -2,6 +2,9 @@
 import sokoenginepy as se
 from services.state_helper import StateHelper
 
+ALL_DIRECTIONS = [se.Direction.RIGHT, se.Direction.DOWN,
+                  se.Direction.LEFT, se.Direction.UP]
+
 class Sokoban(object):
 
     '''Sokoban'''
@@ -9,6 +12,7 @@ class Sokoban(object):
     def __init__(self):
         self.board = self.load_board('levels/level_0.txt')
         self.mover = se.Mover(self.board)
+        self.validate_board()
         return
 
     def load_board(self, level_file):
@@ -18,11 +22,31 @@ class Sokoban(object):
             board = se.SokobanBoard(board_str=''.join(level.readlines()))
         return board
 
+    def validate_board(self):
+        state = se.HashedBoardState(self.board)
+        stack = set(state.goals_positions.values())
+        visited = set()
+        while len(stack) > 0:
+            position = stack.pop()
+            visited.add(position)
+            for direction in ALL_DIRECTIONS:
+                (near, far, near_cell, far_cell) = StateHelper.get_neighbor_cells(self.board, position, direction)
+                if not near_cell.is_wall and not far_cell.is_wall:
+                    if not visited.__contains__(near):
+                        stack.add(near)
+        all_positions = set(self.board._graph.reachables(10, is_obstacle_callable=lambda x: False))
+        deadlocked_positions = all_positions.difference(visited)
+
+        # Apply deadlocks
+        for x in deadlocked_positions:
+            board_cell = self.board.__getitem__(x)
+            board_cell.is_deadlock = True
+
 def main():
     sokoban = Sokoban()
-    print(sokoban.board)
-    StateHelper.take_action(sokoban.mover, se.Direction.DOWN)
-    print(sokoban.board)
+    #print(set(sokoban.board._graph.reachables(1)))
+    #StateHelper.take_action(sokoban.mover, se.Direction.RIGHT)
+    #print(sokoban.board)
     
 
 main()
