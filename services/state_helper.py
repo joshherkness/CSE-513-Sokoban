9de969b2db0081f2cba28d55fee69a3db5ce6fe6
Joseph Herkness <jpherkness@gmail.com>
2017-04-18 14:11:02 -0400

    Added freeze deadlock detection

--- services/state_helper.py
+++ services/state_helper.py
@@ -11,9 +11,7 @@ class StateHelper(object):
         state = se.HashedBoardState(board)
         pusher_id = mover.selected_pusher
         pusher_position = state.pusher_position(pusher_id)
-        neighbor_board_cells = StateHelper.get_neighbor_board_cells(board,
-                                                                    pusher_position,
-                                                                    direction)
+        neighbor_board_cells = StateHelper.near_far_cells(board, pusher_position, direction)
         n_0 = neighbor_board_cells['near']
         n_1 = neighbor_board_cells['far']
 
@@ -30,33 +28,44 @@ class StateHelper(object):
             print("IllegalMoveError risen!")
 
     @staticmethod
-    def get_neighbor_cells(board, position, direction):
+    def near_far_cells(board, position, direction):
         '''
-        retrieves the board cells in the nearest two locations in a direction
-        of a position.
+        returns the nearest two cells in the direction at the specified position.
         '''
-
-        near, far = StateHelper.get_neighbor_positions(board, position, direction)
-        try:
-            near_cell = board.__getitem__(near)
-        except:
-            near_cell = None
-
-        try:
-            far_cell = board.__getitem__(far)
-        except:
-            far_cell = None
-
+        near, far = StateHelper.near_far_positions(board, position, direction)
+        near_cell = board.__getitem__(near) if near is not None else None
+        far_cell = board.__getitem__(far) if far is not None else None
         return near, far, near_cell, far_cell
 
     @staticmethod
-    def get_neighbor_positions(board, position, direction):
+    def near_far_positions(board, position, direction):
         '''
-        retrieves the board cells in the nearest two locations in a direction
-        of a position.
+        returns the positions of the nearest two cells in the direction at the 
+        specified position.
         '''
-
         near = board.neighbor(position, direction)
         far = board.neighbor(near, direction)
         return near, far
 
+    @staticmethod
+    def neighbor_cells(board, position):
+        '''
+        returns the imediate neigbor cells of the specified position.
+        '''
+        up, down, left, right = StateHelper.neighbor_positions(board, position)
+        up_cell = board.__getitem__(up) if up is not None else None
+        down_cell = board.__getitem__(down) if down is not None else None
+        left_cell = board.__getitem__(left) if left is not None else None
+        right_cell = board.__getitem__(right) if right is not None else None
+        return up_cell, down_cell, left_cell, right_cell
+
+    @staticmethod
+    def neighbor_positions(board, position):
+        '''
+        returns the positions of the imediate neigbor cells at the specified position.
+        '''
+        up = board.neighbor(position, se.Direction.UP)
+        down = board.neighbor(position, se.Direction.DOWN)
+        left = board.neighbor(position, se.Direction.LEFT)
+        right = board.neighbor(position, se.Direction.RIGHT)
+        return up, down, left, right
--- sokoban.py
+++ sokoban.py
@@ -1,36 +1,35 @@
-
 import sokoenginepy as se
 from services.state_helper import StateHelper
 
-ALL_DIRECTIONS = [se.Direction.RIGHT, se.Direction.DOWN,
-                  se.Direction.LEFT, se.Direction.UP]
+DIRECTIONS = [se.Direction.RIGHT, se.Direction.DOWN, se.Direction.LEFT, se.Direction.UP]
 
 class Sokoban(object):
 
-    '''Sokoban'''
+    def __init__(self, level_file):
+        '''Creates a sokoban instance and initializes it with a level'''
 
-    def __init__(self):
-        self.board = self.load_board('levels/level_0.txt')
+        self.board = self.load_board(level_file)
         self.mover = se.Mover(self.board)
-        self.validate_board()
-        return
+        self.identify_simple_deadlocks()
 
     def load_board(self, level_file):
-        '''Loads a board'''
+        '''Loads a level from a file into the board instance'''
 
         with open(level_file, 'r') as level:
             board = se.SokobanBoard(board_str=''.join(level.readlines()))
         return board
 
-    def validate_board(self):
+    def identify_simple_deadlocks(self):
+        '''Identifies and marks simple deadlock spaces'''
+
         state = se.HashedBoardState(self.board)
         stack = set(state.goals_positions.values())
         visited = set()
         while len(stack) > 0:
             position = stack.pop()
             visited.add(position)
-            for direction in ALL_DIRECTIONS:
-                (near, far, near_cell, far_cell) = StateHelper.get_neighbor_cells(self.board, position, direction)
+            for direction in DIRECTIONS:
+                (near, far, near_cell, far_cell) = StateHelper.near_far_cells(self.board, position, direction)
                 if not near_cell.is_wall and not far_cell.is_wall:
                     if not visited.__contains__(near):
                         stack.add(near)
@@ -42,9 +41,44 @@ class Sokoban(object):
             board_cell = self.board.__getitem__(x)
             board_cell.is_deadlock = True
 
+    def is_freeze_deadlock(self, position, checked=[]):
+        '''returns a boolean indicating whether or not the block at that position is frozen'''
+        # Remember positions we have already checked to avoid circular checks
+        checked.append(position)
+
+        # Get the immediate neighbors at the position
+        up, down, left, right = StateHelper.neighbor_positions(self.board, position)
+        up_cell, down_cell, left_cell, right_cell = StateHelper.neighbor_cells(self.board, position)
+        
+        # Check if there is a vertical deadlock
+        vertical_deadlock = False
+        if left_cell.is_wall or left in checked or right_cell.is_wall or right in checked:
+            vertical_deadlock = True
+        elif left_cell.is_deadlock and right_cell.is_deadlock:
+            vertical_deadlock = True
+        elif left_cell.has_box or right_cell.has_box:
+            if left_cell.has_box:
+                vertical_deadlock = vertical_deadlock or self.is_freeze_deadlock(left, checked) 
+            if right_cell.has_box:
+                vertical_deadlock = vertical_deadlock or self.is_freeze_deadlock(right, checked) 
+
+        # Check if there is a horizontal deadlock
+        horizontal_deadlock = False
+        if up_cell.is_wall or up in checked or down_cell.is_wall or down in checked:
+            horizontal_deadlock = True
+        elif up_cell.is_deadlock and down_cell.is_deadlock:
+            horizontal_deadlock = True
+        elif up_cell.has_box or down_cell.has_box:
+            if up_cell.has_box:
+                horizontal_deadlock = horizontal_deadlock or self.is_freeze_deadlock(up, checked) 
+            if down_cell.has_box:
+                horizontal_deadlock = horizontal_deadlock or self.is_freeze_deadlock(down, checked) 
+
+        return vertical_deadlock and horizontal_deadlock
+
 def main():
-    sokoban = Sokoban()
-    #print(set(sokoban.board._graph.reachables(1)))
+    sokoban = Sokoban('levels/level_0.txt')
+    print(sokoban.is_freeze_deadlock(20))
     #StateHelper.take_action(sokoban.mover, se.Direction.RIGHT)
     #print(sokoban.board)
     
